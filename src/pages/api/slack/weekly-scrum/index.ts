6d20ab0f95de1c4c70fef5d6cc005ebc9812332f
11t518s <11t518s20161759@gmail.com>
2024-06-19 00:36:48 +0900

    wip

--- src/pages/api/slack/weekly-scrum/index.ts
+++ src/pages/api/slack/weekly-scrum/index.ts
@@ -12,18 +12,20 @@ export default function weeklyScrum(
 ) {
   try {
     // 매일 오전 12시 30분에 스레드를 생성
-    schedule.scheduleJob("26 0 * * *", async () => {
+    schedule.scheduleJob("39 0 * * *", async () => {
       const response = await client.chat.postMessage({
         channel: channelId,
         text: "@here 이번 주 처리할 업무를 공유해주세요.\n\n ex)\n 000 작업 : 몇시간\n000 : 몇시간\n총 주에 몇시간을 이 업무에 투자할 예정",
+        // link_names: true,
       });
       const threadTs = response.ts;
 
       // 같은 날 오전 12시 40분에 댓글을 다는 작업
-      schedule.scheduleJob("27 0 * * *", async () => {
+      schedule.scheduleJob("40 0 * * *", async () => {
         await client.chat.postMessage({
           channel: channelId,
           text: "@here 각자 업무를 확인하고 필요 시 싱크 잡아주세요! 화이팅!",
+          // link_names: true,
           thread_ts: threadTs,
         });
       });
